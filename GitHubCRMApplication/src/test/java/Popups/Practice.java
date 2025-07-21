0d3375b33c5a9d84a2c56521c044ddf7983b7c72
rohini2590 <rohinimuthukrishnan7@gmail.com>
2025-07-21 12:18:26 +0530

    commit1

--- GitHubCRMApplication/src/test/java/Popups/Practice.java
+++ GitHubCRMApplication/src/test/java/Popups/Practice.java
@@ -5,6 +5,14 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("asdfg");
 		System.out.println("12345");
+		System.out.println("etyuii");
+		System.out.println("fhfhjgj");
+		System.out.println("ghfhfgjgjh");
+		System.out.println("tsfgxhgfj");
+		System.out.println("fhfhfyugjgj");
+		System.out.println("ghfhfgjgdfdjh");
+		System.out.println("tsfgxhhjgjggfj");
+		
 
 	}
 
